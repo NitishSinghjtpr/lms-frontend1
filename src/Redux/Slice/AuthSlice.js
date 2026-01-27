95109ca81c596f2fb37eb0972c04d7eddbc3a267
Nitish Kumar <nitishsinghjtpr@gmail.com>
2026-01-27 10:00:09 +0530

    profilr and logout ui create

--- src/Redux/Slice/AuthSlice.js
+++ src/Redux/Slice/AuthSlice.js
@@ -55,7 +55,17 @@ export const login = createAsyncThunk("/auth/login", async (data) => {
 const authSlice = createSlice({
   name: "auth",
   initialState,
-  reducers: {}
+  reducers: {},
+  extraReducers:(builder)=>{
+    builder.addCase(login.fulfilled,(state,action)=>{
+      localStorage.setItem("data",JSON.stringify(action?.payload?.user));
+      localStorage.setItem("isLoggedIn",true);
+      localStorage.setItem("role",action?.payload?.user);
+      state.isLoggedIn=true;
+      state.data=action?.payload?.user;
+      state.role=action?.payload?.user;
+    })
+  }
 });
 
 export default authSlice.reducer;
